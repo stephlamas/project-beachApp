f02df9e7c5fd16bd87ad68108a0beeb517eac15d
Estefania Lamas <esteflamas@gmail.com>
2022-02-14 12:11:56 +0100

    models changed

--- models/User.model.js
+++ models/User.model.js
@@ -1,34 +1,46 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model } = require("mongoose");
 
 const userSchema = new Schema(
   {
     username: {
-      minlength: [5, 'El nombre de usuario debe tener mínimo 5 caracteres'],
+      minlength: [5, "El nombre de usuario debe tener mínimo 5 caracteres"],
       type: String,
       trim: true,
-      required: [true, 'Indica el nombre de usuario'],
-      unique: true
+      required: [true, "Indica el nombre de usuario"],
+      unique: true,
     },
     email: {
       type: String,
-      required: [true, 'Indica el email.'],
+      required: [true, "Indica el email."],
       unique: true,
       lowercase: true,
-      trim: true
+      trim: true,
     },
-    passwordHash: {
+    password: {
       type: String,
-      required: [true, 'Indica la contraseña.']
+      required: [true, "Indica la contraseña."],
     },
     role: {
       type: String,
-      enum: ["user", "admin"],
-      default: "user",
+      enum: ["USER", "ADMIN"],
+      default: "USER",
+    },
+    profile: {
+      name: String,
+      lastName: String,
+      profileImg: {
+        type: String,
+        default: "https://i.stack.imgur.com/l60Hf.png",
+      },
+      favourites: {
+        type: Schema.Types.ObjectId,
+        ref: "Beach",
+      },
     },
   },
   {
-    timestamps: true
+    timestamps: true,
   }
 );
 
-module.exports = model('User', userSchema);
+module.exports = model("User", userSchema);
